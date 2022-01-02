988fde3800693adc93a90b4eb6fe60ee70f6221d
Github Action <sebastc@github.com>
2022-01-02 18:49:05 +0000

    gh-pages

---
+++ precache-manifest.8ea6ab87e11512652e5a0957f7e12ffc.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b093dc9006d1da2ae913",
+    "url": "/gm-emulator/css/app.fcb6c043.css"
+  },
+  {
+    "revision": "4b1a48afc8309a2196ba",
+    "url": "/gm-emulator/css/chunk-vendors.9052c716.css"
+  },
+  {
+    "revision": "1a575a4138e5f366474f0e7c5bd614a5",
+    "url": "/gm-emulator/fonts/fa-brands-400.1a575a41.woff"
+  },
+  {
+    "revision": "513aa607d398efaccc559916c3431403",
+    "url": "/gm-emulator/fonts/fa-brands-400.513aa607.ttf"
+  },
+  {
+    "revision": "592643a83b8541edc52063d84c468700",
+    "url": "/gm-emulator/fonts/fa-brands-400.592643a8.eot"
+  },
+  {
+    "revision": "ed311c7a0ade9a75bb3ebf5a7670f31d",
+    "url": "/gm-emulator/fonts/fa-brands-400.ed311c7a.woff2"
+  },
+  {
+    "revision": "766913e6c0088ab8c9f73e18b4127bc4",
+    "url": "/gm-emulator/fonts/fa-regular-400.766913e6.ttf"
+  },
+  {
+    "revision": "b0e2db3b634d1bc3928e127458d993d8",
+    "url": "/gm-emulator/fonts/fa-regular-400.b0e2db3b.eot"
+  },
+  {
+    "revision": "b91d376b8d7646d671cd820950d5f7f1",
+    "url": "/gm-emulator/fonts/fa-regular-400.b91d376b.woff2"
+  },
+  {
+    "revision": "d1d7e3b4c219fde0f7376c6facfd7149",
+    "url": "/gm-emulator/fonts/fa-regular-400.d1d7e3b4.woff"
+  },
+  {
+    "revision": "0c6bfc668a72935760178f91327aed3a",
+    "url": "/gm-emulator/fonts/fa-solid-900.0c6bfc66.eot"
+  },
+  {
+    "revision": "b9625119ce4300f0ef890a8f3234c773",
+    "url": "/gm-emulator/fonts/fa-solid-900.b9625119.ttf"
+  },
+  {
+    "revision": "d745348d289b149026921f197929a893",
+    "url": "/gm-emulator/fonts/fa-solid-900.d745348d.woff"
+  },
+  {
+    "revision": "d824df7eb2e268626a2dd9a6a741ac4e",
+    "url": "/gm-emulator/fonts/fa-solid-900.d824df7e.woff2"
+  },
+  {
+    "revision": "1d5619cd804367cefe6da2d79289218a",
+    "url": "/gm-emulator/img/fa-brands-400.1d5619cd.svg"
+  },
+  {
+    "revision": "c5d109be8edd3de0f60eb472bd9ef691",
+    "url": "/gm-emulator/img/fa-regular-400.c5d109be.svg"
+  },
+  {
+    "revision": "37bc7099f6f1ba80236164f22e905837",
+    "url": "/gm-emulator/img/fa-solid-900.37bc7099.svg"
+  },
+  {
+    "revision": "964b72b504dcf101d7475633fbfdf53b",
+    "url": "/gm-emulator/index.html"
+  },
+  {
+    "revision": "7e830cacb917cf24da56",
+    "url": "/gm-emulator/js/about.156802c0.js"
+  },
+  {
+    "revision": "b093dc9006d1da2ae913",
+    "url": "/gm-emulator/js/app.347c630e.js"
+  },
+  {
+    "revision": "4b1a48afc8309a2196ba",
+    "url": "/gm-emulator/js/chunk-vendors.c8e3ac8f.js"
+  },
+  {
+    "revision": "745c2612f41f7ac51b88632b901477d2",
+    "url": "/gm-emulator/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/gm-emulator/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/gm-emulator/precache-manifest.8ea6ab87e11512652e5a0957f7e12ffc.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "gm-emulator"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
